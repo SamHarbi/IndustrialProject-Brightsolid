a6492ea684193d9895be98162ae52d7d686d2212
Sameer <szalharbi@dundee.ac.uk>
2022-10-06 15:29:45 +0100

    Add Test Connection to route instead

--- Backend/index.js
+++ Backend/index.js
@@ -39,23 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//Database Connection
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  port: 25060,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database:  'defaultdb'
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-});
-
-connection.end();
 
 module.exports = app;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,6 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+
+//Database Connection
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: 25060,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database:  'defaultdb'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+});
+
+connection.end();
+
   res.send('respond with a resource');
 });
 
