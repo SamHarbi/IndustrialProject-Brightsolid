a25be1c947e4fab48be8c5828b1f40b7117de33e
Sameer <szalharbi@dundee.ac.uk>
2022-10-06 14:09:49 +0100

    Clear Function based backend

--- Backend/Functions/packages/ComplianceManager/hello/hello.js
+++
@@ -1,7 +0,0 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello World! Nice to meet you' + name + '!' + + 'This is a Compliance Manager route.'
-    console.log(greeting)
-    return {"body": greeting}
-  }
-  
--- Backend/Functions/packages/Shared/hello/hello.js
+++
@@ -1,7 +0,0 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello World! Nice to meet you' + name + '!' + 'This is a shared route.'
-    console.log(greeting)
-    return {"body": greeting}
-  }
-  
