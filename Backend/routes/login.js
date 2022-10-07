6ffeb5739e4b3153a2c1a35d25ec46bf225b87b8
Sameer <szalharbi@dundee.ac.uk>
2022-10-07 14:20:24 +0100

    Add Passport.js Auth

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,5 +1,10 @@
 var express = require('express');
 const mysql = require('mysql2');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var crypto = require('crypto');
+
 var router = express.Router();
 
 connectionSetup = require('../database.js');
@@ -11,9 +16,24 @@ router.post('/', function(req, res, next) {
 connection = connectionSetup.databaseSetup();
 
 connection.connect();
-/*
-  LOGIN TODO HERE
-*/
+
+//From the Passport.js Documentation
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+
+  connection.query('SELECT * FROM users WHERE username = ?', (err, row, fields) => { 
+    if (err) { res.send('Something went wrong :(');}
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+      return cb(null, row);
+    });
+  });
+}));
+
 connection.end();
 
 res.send('respond with a resource');
