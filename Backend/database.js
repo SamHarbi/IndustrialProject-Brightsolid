5872f886f42d3da57691901e19b699330dd91f94
Sameer <szalharbi@dundee.ac.uk>
2022-10-08 18:03:29 +0100

    Signup page testing

--- Backend/database.js
+++ Backend/database.js
@@ -1,21 +1,21 @@
 //Unified Source for connecting to the database packaged as a module that can be imported and called by each route 
-exports.databaseSetup = function ()  {
-    
+exports.databaseSetup = function () {
+
     const mysql = require('mysql2');
 
     //Database Connection
     const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 25060,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database:  'defaultdb'
+        host: process.env.HOST,
+        port: 25060,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: 'defaultdb'
     });
 
     return connection;
 };
-    
-    
+
+
 
 
 
