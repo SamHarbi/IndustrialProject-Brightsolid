7f27d70698c855999db68095620c05e0e56db24f
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-09 17:51:09 +0100

    Stop redirection on login

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -49,7 +49,7 @@ router.post('/', express.urlencoded({ extended: false }), function (req, res, ne
       req.session.user = req.body.user
       req.session.save(function (err) {
         if (err) { return next(err) }
-        res.redirect('/')
+        //res.redirect('/')
       })
     })
   });
