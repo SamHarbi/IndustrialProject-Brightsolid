3baf99404f7a95cb122b60b8b58665536b5097b8
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-09 17:58:33 +0100

    General Debugging

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -50,7 +50,7 @@ router.post('/', express.urlencoded({ extended: false }), function (req, res, ne
       req.session.user = req.body.user
       req.session.save(function (err) {
         if (err) { return next(err) }
-        res.return("Done")
+        res.send("Done");
       })
     })
   });
