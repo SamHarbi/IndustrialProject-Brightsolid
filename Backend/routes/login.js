02fe96c77aedc670c7410cb0c7689e92a1d08af1
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-09 18:35:03 +0100

    General Debugging

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -46,21 +46,19 @@ function isAuthenticated(req, res, next) {
 
 /* POST users listing. */
 router.post('/', express.urlencoded({ extended: false }), function (req, res, next) {
-  try {
-    login(req.body.username, req.body.password).then((value) => {
-      console.log(value);
-      req.session.regenerate(function (err) {
-        if (err) { next(err); }
-        req.session.user = req.body.user
-        req.session.save(function (err) {
-          if (err) { return next(err) }
-          res.send("Done");
-        })
+  login(req.body.username, req.body.password).then((value) => {
+    console.log(value);
+    req.session.regenerate(function (err) {
+      if (err) { next(err); }
+      req.session.user = req.body.user
+      req.session.save(function (err) {
+        if (err) { return next(err) }
+        res.send("Done");
       })
-    });
-  } catch (err) {
-    res.send(err);
-  }
+    })
+  }).catch(function () {
+    console.log("Error");
+  });
 });
 
 module.exports = router;
