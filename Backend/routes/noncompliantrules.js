60f108f84206d4a447852e37de8f22c65c5bf1c1
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-12 12:55:54 +0100

    Add detailreport route

--- Backend/routes/noncompliantrules.js
+++ Backend/routes/noncompliantrules.js
@@ -15,22 +15,99 @@ function isAuthenticated(req, res, next) {
     else next('/')
 }
 
-/* GET listing. */
-router.get('/', isAuthenticated, function (req, res) {
-
-    var returnData = [];
-
-    connection.query('SELECT * FROM `resource` WHERE resource_id IN (SELECT resource_id FROM non_compliance) AND account_id = ?; ', [req.session.accountID], (err, row, fields) => {
-        if (err) { //Query didn't run
-            res.send('Something went wrong :(');
-        }
-        if (row.length < 1) {//No result from query
-            res.send('No Resource Found');
-        } else {
-            res.json(row); //Return result
-        }
+function getNonCompliantResource(req) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM `resource` WHERE resource_id IN (SELECT resource_id FROM non_compliance WHERE rule_id = ?) AND account_id = ?; ', [req.body.id], [req.session.accountID], (err, row, fields) => {
+            if (err) { //Query didn't run
+                reject('Something went wrong :(');
+            }
+            if (row.length < 1) {//No result from query
+                reject('No Resource Found');
+            } else {
+                resolve(row); //Return result
+            }
+        });
     });
+}
 
+//SELECT * FROM `resource` WHERE resource_type_id IN (SELECT resource_type_id FROM rule WHERE rule_id NOT IN (SELECT rule_id FROM non_compliance WHERE rule_id NOT IN (SELECT rule_id FROM exception) ) AND rule_id = ?) AND account_id = ?; 
+function getCompliantResource(req) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM `resource` WHERE resource_type_id IN (SELECT resource_type_id FROM rule WHERE rule_id NOT IN (SELECT rule_id FROM non_compliance WHERE rule_id = ?) AND account_id = ?;', [req.body.id], [req.session.accountID], (err, row, fields) => {
+            if (err) { //Query didn't run
+                reject('Something went wrong :(');
+            }
+            if (row.length < 1) {//No result from query
+                reject('No Resource Found');
+            } else {
+                resolve(row); //Return result
+            }
+        });
+    });
+}
+
+function getException(req) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM exception WHERE rule_id = ? AND customer_id IN (SELECT customer_id FROM account WHERE account_id = ?)', [req.body.id], [req.session.accountID], (err, row, fields) => {
+            if (err) { //Query didn't run
+                reject('Something went wrong :(');
+            }
+            if (row.length < 1) {//No result from query
+                reject('No Resource Found');
+            } else {
+                resolve(row); //Return result
+            }
+        });
+    });
+}
+
+async function processResults(req) {
+    //Get Data from DB
+    var nonCompliant = await getNonCompliantResource(req);
+    var compliant = await getCompliantResource(req);
+    var exception = await getException(req);
+
+    //Define a new json response array
+    var data = [];
+
+    //Fill the array with non compliant resources and format the data
+    for (let i = 0; i < nonCompliant.length; i++) {
+        data.push({
+            id: (nonCompliant[i].rule_id).toString(),
+            resource: nonCompliant[i].rule_name,
+            complianceState: "Non-Compliant",
+            complianceStateID: 0, //Makes it easier to figure out in the frontend for calculation 
+            exception: "NA",
+            justification: "NA",
+            reviewdate: "NA",
+            lastupdated: "NA",
+            updatedby: "NA"
+        })
+    }
+
+    //Fill the array with compliant resources and format the data
+    for (let i = 0; i < compliant.length; i++) {
+        data.push({
+            id: (compliant[i].rule_id).toString(),
+            ruleName: compliant[i].rule_name,
+            complianceState: "Compliant",
+            complianceStateID: 1, //Makes it easier to figure out in the frontend for calculation 
+            exception: exception.exception_value,
+            justification: exception.justification,
+            reviewdate: exception.review_date,
+            lastupdated: exception.last_updated,
+            updatedby: exception.last_updated_by
+        })
+    }
+
+    return data;
+}
+
+/* POST listing. */
+router.post('/', isAuthenticated, function (req, res) {
+    processResults(req).then((data) => {
+        res.json(data);
+    })
 })
 
 module.exports = router;
