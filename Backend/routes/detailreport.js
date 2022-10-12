0491061fb19801d162280c293ff4489e7420ed97
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-12 17:34:02 +0100

    Add debugging info for detail report route

--- Backend/routes/detailreport.js
+++ Backend/routes/detailreport.js
@@ -19,9 +19,11 @@ function getNonCompliantResource(req) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM `resource` WHERE resource_id IN (SELECT resource_id FROM non_compliance WHERE rule_id = ?) AND account_id = ?;', [req.body.id], [req.session.accountID], (err, row, fields) => {
             if (err) { //Query didn't run
+                console.log("Reject 1");
                 reject('Something went wrong :(');
             }
             if (row.length < 1) {//No result from query
+                console.log("Reject 2");
                 reject('No Resource Found');
             } else {
                 resolve(row); //Return result
@@ -35,9 +37,11 @@ function getCompliantResource(req) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM `resource` WHERE resource_type_id IN (SELECT resource_type_id FROM rule WHERE rule_id NOT IN (SELECT rule_id FROM non_compliance WHERE rule_id = ?)) AND account_id = ?;', [req.body.id], [req.session.accountID], (err, row, fields) => {
             if (err) { //Query didn't run
+                console.log("Reject 3");
                 reject('Something went wrong :(');
             }
             if (row.length < 1) {//No result from query
+                console.log("Reject 4");
                 reject('No Resource Found');
             } else {
                 resolve(row); //Return result
@@ -50,9 +54,11 @@ function getException(req) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM exception WHERE rule_id = ? AND customer_id IN (SELECT customer_id FROM account WHERE account_id = ?)', [req.body.id], [req.session.accountID], (err, row, fields) => {
             if (err) { //Query didn't run
+                console.log("Reject 5");
                 reject('Something went wrong :(');
             }
             if (row.length < 1) {//No result from query
+                console.log("Reject 6");
                 reject('No Resource Found');
             } else {
                 resolve(row); //Return result
@@ -62,8 +68,6 @@ function getException(req) {
 }
 
 async function processResults(req) {
-
-    console.log(req.body.id);
 
     //Get Data from DB
     var nonCompliant = await getNonCompliantResource(req);
