2a6e8ff962f8cdeec82d83ab3194e091fc06fe65
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-13 17:27:01 +0100

    fix bugs with exception creation

--- Backend/routes/createexception.js
+++ Backend/routes/createexception.js
@@ -16,14 +16,26 @@ function isAuthenticated(req, res, next) {
     else next('/')
 }
 
+/*
+    It is pretty clear that a non atomic commit kind of event can occur here that is why the frontend autofills all information on the 
+    users behalf (gotten from the server beforehand through other routes) to ensure it is correct, the only item dependant on the user, 
+    resource_ID is checked by trying to delete it. Information is not autofilled in the testexceptioncreate page
+*/
 function createException(req) {
-    return new Promise((resolve, reject) => {
-        connection.query('INSERT INTO exception (customer_id, rule_id, last_updated_by, exception_value, justification, resource_id) VALUES ((SELECT customer_id FROM account WHERE account_id = ? ), ?, ?, (SELECT resource_name FROM resource WHERE resource_id = ?), ?, ?);', [req.session.accountID, req.body.ruleID, req.session.accountID, req.body.resourceID, req.body.justification, req.body.resourceID], (err, row, fields) => {
+    return new Promise((resolve, reject) => { //First check that exception is not compliant and then delete it
+        connection.query('DELETE FROM non_compliance WHERE resource_id = ?;', [req.body.resourceID], (err, row, fields) => {
             if (err) { //Query didn't run
                 reject(err);
             }
-            else {
-                resolve("Exception Created");
+            else { //Then add exception
+                connection.query('INSERT INTO exception (customer_id, rule_id, last_updated_by, exception_value, justification, resource_id) VALUES ((SELECT customer_id FROM account WHERE account_id = ? ), ?, ?, (SELECT resource_name FROM resource WHERE resource_id = ?), ?, ?);', [req.session.accountID, req.body.ruleID, req.session.accountID, req.body.resourceID, req.body.justification, req.body.resourceID], (err, row, fields) => {
+                    if (err) { //Query didn't run
+                        reject(err);
+                    }
+                    else {
+                        resolve("Exception Created");
+                    }
+                });
             }
         });
     });
