bd1c5f5daf1978bbf2a837ff0dd5c2fdc27f070b
sam <szalharbi@dundee.ac.uk>
2022-10-14 13:12:43 +0100

    Add a route to get username

---
+++ Backend/routes/whoamI.js
@@ -0,0 +1,24 @@
+/*
+A Secured Route that takes a GET request and returns some HTML from the express-session documentation to print the user's name and id
+Can be used to test if a session is active
+*/
+require('dotenv').config() //.env files for local testing
+
+var express = require('express');
+var session = require('express-session')
+var escapeHtml = require('escape-html')
+const mysql = require('mysql2');
+var router = express.Router();
+
+// middleware to test if authenticated - copied from https://www.npmjs.com/package/express-session user login example
+function isAuthenticated(req, res, next) {
+    if (req.session.user) next()
+    else next('/')
+}
+
+/* GET users listing. */
+router.get('/', isAuthenticated, function (req, res) {
+    res.json({ user: req.session.user });
+})
+
+module.exports = router;
