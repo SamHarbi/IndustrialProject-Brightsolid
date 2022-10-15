b9208a3ce227c1ffbf532e81351d4612eaeac253
Sam <55399333+SamHarbi@users.noreply.github.com>
2022-10-15 16:59:20 +0100

    Refactor of how exception data is created and updated

--- Backend/routes/createexception.js
+++ Backend/routes/createexception.js
@@ -15,6 +15,8 @@ dayjs.extend(customParseFormat);
 
 let oldExceptionsPresent = false;
 
+let exceptionAction = 0;
+
 // middleware to test if authenticated - copied from https://www.npmjs.com/package/express-session user login example
 function isAuthenticated(req, res, next) {
     if (req.session.user) next()
@@ -26,31 +28,69 @@ function isAuthenticated(req, res, next) {
     users behalf (gotten from the server beforehand through other routes) to ensure it is correct, the only item dependant on the user, 
     resource_ID is checked by trying to delete it. Information is not autofilled in the testexceptioncreate page
 */
-// This function creates the exception
-function createException(req) {
+// This function removes the resource from non compliance and gets if any exception record
+function stageException(req) {
     return new Promise((resolve, reject) => { //First check that exception is not compliant and then delete it
         connection.query('DELETE FROM non_compliance WHERE resource_id = ? AND resource_id IN (SELECT resource_id FROM resource WHERE account_id = ?);', [req.body.resourceID, req.body.accountID], (err, row, fields) => {
             if (err) { //Query didn't run
                 reject(err);
             }
-            else { //Then add exception
-
-                //Setup Time to be added
-                let lastUpdate = dayjs();
-                lastUpdate = lastUpdate.format("YYYY-MM-DD hh:mm:ss");
-                reviewDate = dayjs().add(req.body.addedTime, 'month');
-                reviewDate = reviewDate.format("YYYY-MM-DD hh:mm:ss");
-
-                connection.query('INSERT INTO exception (customer_id, rule_id, last_updated_by, exception_value, justification, resource_id, review_date, last_updated, active) VALUES ((SELECT customer_id FROM account WHERE account_id = ? ), ?, ?, (SELECT resource_name FROM resource WHERE resource_id = ?), ?, ?, ?, ?, 1);', [req.body.accountID, req.body.ruleID, req.session.accountID, req.body.resourceID, req.body.justification, req.body.resourceID, reviewDate, lastUpdate], (err, row, fields) => {
+            else { //Find an exception if any
+                connection.query('SELECT * FROM exception WHERE resource_id = ? AND rule_id = ?', [req.body.resourceID, req.body.ruleID], (err, row, fields) => {
                     if (err) { //Query didn't run
                         reject(err);
                     }
                     else {
-                        resolve("Exception Created");
+                        resolve(row);
                     }
                 });
             }
         });
+    });
+}
+
+//This function either creates or updates an existing exception record 
+function createException(req, exep) {
+    return new Promise((resolve, reject) => {
+
+        //Setup Time to be added
+        let lastUpdate = dayjs();
+        lastUpdate = lastUpdate.format("YYYY-MM-DD hh:mm:ss");
+        reviewDate = dayjs().add(req.body.addedTime, 'month');
+        reviewDate = reviewDate.format("YYYY-MM-DD hh:mm:ss");
+
+        if (exep.length > 0) { //current exception found - Update it
+
+            connection.query('UPDATE exception SET last_updated_by = ?, justification = ?, review_date = ?, last_updated = ?, active = 1 WHERE exception_id = ?', [req.session.accountID, req.body.justification, reviewDate, lastUpdate, exep[0].exception_id], (err, row, fields) => {
+                if (err) { //Query didn't run
+                    reject(err);
+                }
+                else {
+                    exceptionAction = -1;
+                    resolve(exep);
+                }
+            });
+
+        } else { //No current exception - Create it
+
+            //Then add an exception
+            connection.query('INSERT INTO exception (customer_id, rule_id, last_updated_by, exception_value, justification, resource_id, review_date, last_updated, active) VALUES ((SELECT customer_id FROM account WHERE account_id = ? ), ?, ?, (SELECT resource_name FROM resource WHERE resource_id = ?), ?, ?, ?, ?, 1);', [req.body.accountID, req.body.ruleID, req.session.accountID, req.body.resourceID, req.body.justification, req.body.resourceID, reviewDate, lastUpdate], (err, row, fields) => {
+                if (err) { //Query didn't run
+                    reject(err);
+                }
+                else { //Find the newly made exception and return it
+                    connection.query('SELECT * FROM exception WHERE resource_id = ? AND rule_id = ?', [req.body.resourceID, req.body.ruleID], (err, row, fields) => {
+                        if (err) { //Query didn't run
+                            reject(err);
+                        }
+                        else {
+                            exceptionAction = 1;
+                            resolve(row);
+                        }
+                    });
+                }
+            });
+        }
     });
 }
 
@@ -69,21 +109,7 @@ function getOldExceptionData(req) {
     });
 }
 
-//This function gets the exception that was most recently created for an account
-function getNewExceptionData(req) {
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM exception WHERE exception_id IN (SELECT max(exception_id) FROM exception WHERE customer_id IN (SELECT customer_id FROM account WHERE account_id = ? ))', [req.body.accountID], (err, row, fields) => {
-            if (err) { //Query didn't run
-                reject(err);
-            }
-            else {
-                resolve(row);
-            }
-        });
-    });
-}
-
-//Creates the audit using exception data gotten with the previous, getNewExceptionData in tandem with post data
+//Creates the audit using exception data passed to it
 function createAudit(req, exep, oldexep) {
     return new Promise((resolve, reject) => {
 
@@ -95,7 +121,8 @@ function createAudit(req, exep, oldexep) {
         let lastUpdate = dayjs();
         lastUpdate = lastUpdate.format("YYYY-MM-DD hh:mm:ss");
 
-        if (oldExceptionsPresent == true) {
+        if (exceptionAction == -1) {
+            exceptionAction = 0;
             ruleAction = "update";
             connection.query(que, [exep[0].exception_id, req.body.accountID, req.session.accountID, req.body.ruleID, ruleAction, lastUpdate, oldexep[0].new_exception_value, exep[0].exception_value, oldexep[0].new_justification, req.body.justification, oldexep[0].new_review_date, lastUpdate], (err, row, fields) => {
                 if (err) { //Query didn't run
@@ -133,26 +160,21 @@ async function processResults(req) {
 
     try {
         oldExceptionData = await getOldExceptionData(req);
-        if (oldExceptionData.length > 0) {
-            oldExceptionsPresent = true;
-        } else {
-            oldExceptionsPresent = false;
-        }
     } catch (err) {
         console.log("No Previous Exceptions");
     }
 
     try {
-        exception = await createException(req);
+        exception = await stageException(req);
     } catch (err) {
-        data = "Error in Exception";
+        data = "Error in staging Exception";
         console.log(err);
     }
 
     try {
-        exceptionData = await getNewExceptionData(req);
+        exceptionData = await createException(req, exception);
     } catch (err) {
-        data = "Error in getting Exception";
+        data = "Error in creating Exception";
         console.log(err);
     }
 
